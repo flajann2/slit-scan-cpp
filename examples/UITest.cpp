7402476b9bc7fbdeb58f3071eee770a031835b4d
El Toro Maestro <eltoro@atomlogik.de>
2024-09-07 10:54:15 -0500

    Signal and slot works. Had to wire them manually. The usual way did not work for C++

--- examples/UITest.cpp
+++ examples/UITest.cpp
@@ -2,6 +2,7 @@
 #include <gtkmm/application.h>
 #include <gtkmm/builder.h>
 #include <gtkmm/window.h>
+#include <gtkmm/button.h>
 #include <giomm/menuitem.h>
 #include <glibmm.h>
 #include <giomm.h>
@@ -9,41 +10,76 @@
 #include <giomm/simpleaction.h>
 #include <iostream>
 
+using namespace std;
+
+extern "C" void main_save() {
+  cout << "main_save called in global scope" << endl;
+}
+
+class UITestApplication : public Gtk::Application {
+public:
+  UITestApplication(string appname = "de.atomlogik.uitest")
+    : Gtk::Application(appname) {
+  }
+   
+protected:
+
+  virtual void on_startup() override {
+    Gtk::Application::on_startup();
+    auto builder = Gtk::Builder::create_from_file("uitest");
+    main_window = builder->get_object<Gtk::Window>("main_window");
+    main_window->show();
+    add_window(*main_window);
+    cout << "window title: " << main_window->get_title() << endl;
+
+    // get what we need to attache event handlers to.
+    main_save = builder->get_object<Gtk::Button>("main_save");
+    main_save_as = builder->get_object<Gtk::Button>("main_save_as");
+    main_quit = builder->get_object<Gtk::Button>("main_quit");
+    
+    // Connect signals to handlers (slots?)
+    main_save->signal_clicked().connect(sigc::mem_fun(*this, &UITestApplication::on_main_save));
+    main_save_as->signal_clicked().connect(sigc::mem_fun(*this, &UITestApplication::on_main_save_as));
+    main_quit->signal_clicked().connect(sigc::mem_fun(*this, &UITestApplication::on_main_quit));
+  }
+  
+  void on_main_save() {
+    cout << "main_save called" << endl;
+  }
+
+  void on_main_save_as() {
+    cout << "main_save_as called" << endl;
+  }
+
+  void on_main_quit() {
+    cout << "main_quit called" << endl;
+    quit();
+  }
+private:
+  shared_ptr<Gtk::Window> main_window;  
+  shared_ptr<Gtk::Button> main_save;
+  shared_ptr<Gtk::Button> main_save_as;
+  shared_ptr<Gtk::Button> main_quit;
+};
+
 int main(int argc, char* argv[]) {
-    auto app = Gtk::Application::create("org.example.menuapp");
+  UITestApplication app;
+    
+  /// // Load the UI from the Glade file
+  /// auto builder = Gtk::Builder::create();
+  /// builder->add_from_file("uitest");
+  
+  /// // Get references to the window and menu bar
+  /// auto window = builder->get_object<Gtk::Window>("main_window");
+  /// //auto menubar = builder->get_object<Gio::Menu>("menubar");
+  
+  /// // Create menu items using the builder
 
-    // Load the UI from the Glade file
-    auto builder = Gtk::Builder::create();
-    builder->add_from_file("uitest");
+  /// // Connect the quit menu item to the application's quit function
+  /// auto action = Gio::SimpleAction::create("quit-action");
 
-    // Get references to the window and menu bar
-    auto window = builder->get_object<Gtk::Window>("main_window");
-    auto menubar = builder->get_object<Gio::Menu>("menubar");
+  /// window->show();
 
-    // Create menu items using the builder
-    auto file_menu_item = builder->get_object<Gio::MenuItem>("file_item");
-    auto quit_menu_item = builder->get_object<Gio::MenuItem>("quit_item");
-
-    // Connect the quit menu item to the application's quit function
-    auto action = Gio::SimpleAction::create("quit-action");
-    /// action->signal_activate().connect([&app]() {
-    ///     // Handle the action here
-    ///     std::cout << "Quit Action activated!" << std::endl;
-    ///     app->quit();
-    /// });
-    /// quit_menu_item->set_action_and_target(action->get_name()
-    ///                                       , Glib::Variant<std::string>("quit_target_value"));
-
-    /// quit_menu_item->signal_activate().connect(
-    ///     [&app]() { app->quit(); }
-    /// );
-
-    // Add the menu items to the menu bar
-    // file_menu_item->set_submenu(menubar->get_submenu());
-
-    window->set_default_size(300, 200);
-    window->show();
-
-    return app->run();
+  return app.run(argc, argv);
 }
 

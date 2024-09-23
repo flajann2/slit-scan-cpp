bd011dbbc9ce112016cbba211cfb7b056232e2e5
El Toro Maestro <eltoro@atomlogik.de>
2024-09-23 16:33:39 -0500

    working on a jthread solution for the image processing

--- gels/include/gels.h
+++ gels/include/gels.h
@@ -3,11 +3,13 @@
 
 #include <opencv2/opencv.hpp>
 
-class GelSource {
-public:
-  virtual void signal_next_frame() = 0;
-
-protected:
-  virtual void broadcast_next_frame(cv::Mat frame) {
-  }
-};
+namespace Gel {
+  class GelSource {
+  public:
+    virtual void signal_next_frame() = 0;
+    
+  protected:
+    virtual void broadcast_next_frame(cv::Mat frame) {
+    }
+  };
+}
